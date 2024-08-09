a6418508b5daf287ae48f379992f8360481677d9
小雨 <zsfys225011250@qq.com>
2024-08-09 14:31:37 +0000

    UP
    Signed-off-by: 小雨 <zsfys225011250@qq.com>

--- install.sh
+++ install.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
+#颜色变量
+Hong='\033[0;31m'  # 红色
+Lan='\033[0;34m'   # 蓝色
+Huang='\033[0;33m' # 黄色
+Lu='\033[0;32m'    # 绿色
+Zi='\033[0;35m'    # 紫色
+Fen='\033[0;95m'   # 粉色
+#恢复颜色
+RESET_COLOR='\033[0m'
 architecture=$(uname -m)
 if [ "$architecture" == "x86_64" ]; then
-echo "当前架构为AMD"
+echo -e "${Lan}当前架构为AMD${RESET_COLOR}"
 else
-echo "当前架构为ARM"
+echo -e "${Hong}当前架构为ARM${RESET_COLOR}"
+fi
+
+# 检查git是否已经安装
+if command -v git >/dev/null 2>&1; then
+echo -e "${Huang}Git已经安装，跳过安装步骤${RESET_COLOR}"
+else
+echo -e "${Hong}Git未安装，开始进行安装${RESET_COLOR}"
+sudo apt install -y git
+echo -e "${Lan}Git安装完成${RESET_COLOR}"
 fi
 
 distro=$(lsb_release -si)
@@ -13,51 +31,51 @@ minor_version=$(echo $version | cut -d. -f2)
     
 # 判断是否为Ubuntu系统且主版本号大于等于20
 if [ "$distro" = "Ubuntu" ] && [ $major_version -ge 20 ]; then
-echo "当前Ubuntu版本为：$version "
+echo -e "${Lu}当前Ubuntu版本为：$version{RESET_COLOR}"
 else
-echo '当前非 Ubuntu，请更换Ubuntu发行版再试'
+echo -e "${Hong}⚠️⚠️⚠️：当前非 Ubuntu，请更换Ubuntu发行版再试${RESET_COLOR}"
 exit 0
 fi
 
-echo "请选择是否更新apt列表和安装必要依赖包"
-echo "初次安装使用请yes (yes或no)"
+echo -e "${Zi}请选择是否更新apt列表和安装必要依赖包"
+echo -e "初次安装使用请yes (yes或no)${RESET_COLOR}"
 
 while true; do
 read user_input
 if [ "$user_input" == "yes" ] || [ "$user_input" == "y" ]; then
-echo "正在更新apt列表和升级已安装的软件包"
+echo -e "${Lu}正在更新apt列表和升级已安装的软件包${RESET_COLOR}"
 sudo apt update -y
 sudo apt upgrade -y
 # 安装必要的包，不可跳过
-echo "正在安装必要依赖包中..."
+echo -e "${Lu}正在安装必要依赖包中...${RESET_COLOR}"
 sudo apt install -y apt-transport-https curl ca-certificates software-properties-common
-echo "更新成功"
+echo -e "${Lan}更新成功${RESET_COLOR}"
 break
 elif [ "$user_input" == "no" ] || [ "$user_input" == "n" ]; then
 break
 else
-echo "输入错误，请输入yes或no:"
+echo -e "${Hong}输入错误，请输入yes或no:${RESET_COLOR}"
 fi
 done
 
 # 检查ffmpeg是否已经安装
 if command -v ffmpeg >/dev/null 2>&1; then
-echo "ffmpeg已经安装，跳过安装步骤"
+echo -e "${Huang}ffmpeg已经安装，跳过安装步骤${RESET_COLOR}"
 else
-echo "ffmpeg未安装，开始进行安装"
+echo -e "${Hong}ffmpeg未安装，开始进行安装${RESET_COLOR}"
 sudo apt install -y ffmpeg
-echo "ffmpeg安装完成"
+echo -e "${Lan}ffmpeg安装完成${RESET_COLOR}"
 fi
 
 # 检查nodejs是否已经安装
 if [ -z "$(command -v node)" ]; then
-echo "nodejs未安装，开始安装..."
+echo -e "${Huang}nodejs未安装，开始安装...${RESET_COLOR}"
 # 安装nodejs
 sudo apt remove -y libnode-dev
 curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt install -y nodejs
 else
-echo "nodejs已安装，检查版本..."
+echo -e "${Huang}nodejs已安装，检查版本...${RESET_COLOR}"
 # 获取nodejs的版本信息
 NODE_VERSION=$(node -v | grep -oE "[0-9]+\.[0-9]+\.[0-9]+")
 IFS='.' read -r major minor patch <<< "$NODE_VERSION"
@@ -67,7 +85,7 @@ patch=$((10#$patch))
 
 # 判断版本是否大于或等于20
 if [[ $major -lt 20 ]]; then
-echo "nodejs版本小于20，重新安装nodejs20中..."
+echo -e "${Hong}nodejs版本小于20，重新安装nodejs20中...${RESET_COLOR}"
 # 安装nodejs
 sudo apt remove -y libnode-dev
 curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
@@ -75,107 +93,98 @@ sudo apt install -y nodejs
 else
 # 判断版本是否小于或者等于22
 if [[ $major -lt 22 ]]; then
-echo "nodejs版本小于22，当前版本号为：$NODE_VERSION"
+echo -e "${Lu}nodejs版本小于22，当前版本号为：$NODE_VERSION"
 echo "您可以选择继续使用当前稳定版本不进行升级为22非稳定版本"
-echo "请选择是否安装（yes/no）"
+echo "请选择是否安装（yes/no）${RESET_COLOR}"
 # yes/n判断
 while true; do
 read user_input
 if [ "$user_input" == "yes" ] || [ "$user_input" == "y" ]; then
-echo '正在安装中'
+echo -e "${Hong}正在安装中${RESET_COLOR}"
 sudo apt remove -y libnode-dev
 curl -sL https://deb.nodesource.com/setup_22.x | sudo -E bash -
 sudo apt install -y nodejs
-echo '安装完毕'
+echo -e "${Lan}安装完毕${RESET_COLOR}"
 break
 elif [ "$user_input" == "no" ] || [ "$user_input" == "n" ]; then
 break
 else
-echo "输入错误，请输入yes或no:"
+echo -e "${Hong}输入错误，请输入yes或no:${RESET_COLOR}"
 fi
 done
 else
-echo "当前node版本：$NODE_VERSION，无需升级"
+echo -e "${Lu}当前node版本：$NODE_VERSION，无需升级${RESET_COLOR}"
 fi
 fi
 fi
 
 # 检查pnpm是否已安装
 if ! type pnpm >/dev/null 2>&1; then
-echo 'pnpm未安装'
-echo '正在安装pnpm中'
+echo -e "${Hong}pnpm未安装"
+echo -e "${Hong}正在安装pnpm中${RESET_COLOR}"
 # 安装pnpm
 npm --registry=https://registry.npmmirror.com install pnpm -g
 else
-echo 'pnpm已安装，跳过安装步骤'
-fi
-
-# 检查git是否已经安装
-if command -v git >/dev/null 2>&1; then
-echo "Git已经安装，跳过安装步骤"
-else
-echo "Git未安装，开始进行安装"
-sudo apt install -y git
-echo "Git安装完成"
+echo -e "${Huang}pnpm已安装，跳过安装步骤${RESET_COLOR}"
 fi
 
 # 检查redis-server是否已安装
 if dpkg -l | grep -q "^ii  redis-server"; then
-echo "redis-server已安装，跳过安装步骤"
+echo -e "${Huang}redis-server已安装，跳过安装步骤${RESET_COLOR}"
 if [ $(which systemctl) ] && [ $(systemctl is-enabled redis-server) == "enabled" ]; then
-echo "redis已开启自启，取消设置"
+echo -e "${Huang}redis已开启自启，取消设置${RESET_COLOR}"
 else
 sudo systemctl enable redis-server
-echo '设置redis自启服务'
+echo -e "${Hong}已设置redis自启服务${RESET_COLOR}"
 fi
 else
-echo "正在安装redis数据库"
+echo -e "${Hong}正在安装redis数据库${RESET_COLOR}"
 sudo apt install -y redis-server
-echo "正在设置redis自启服务"
+echo -e "${Hong}正在设置redis自启服务${RESET_COLOR}"
 sudo systemctl enable redis-server
-echo '设置成功'
+echo -e "${Lan}设置成功${RESET_COLOR}"
 fi
 
 if dpkg -l | grep -q "^ii  fonts-wqy-microhei"; then
-echo "fonts-wqy-microhei已安装，跳过安装步骤"
-echo '中文字体安装完毕'
+echo -e "${Huang}fonts-wqy-microhei已安装，跳过安装步骤${RESET_COLOR}"
+echo -e "${Lan}中文字体安装完毕${RESET_COLOR}"
 else
-echo '正在安装中文字体-文泉驿微米黑中'
+echo -e "${Hong}正在安装中文字体-文泉驿微米黑中${RESET_COLOR}"
 sudo apt install -y fonts-wqy-microhei
-echo "fonts-wqy-microhei安装完成"
+echo -e "${Lan}fonts-wqy-microhei安装完成${RESET_COLOR}"
 fi
 
-echo "请选择是否重置字体缓存，初次安装请yes(yes或no)"
+echo -e "${Zi}请选择是否重置字体缓存，初次安装请yes(yes或no)${RESET_COLOR}"
 
 while true; do
 read user_input
 if [ "$user_input" == "yes" ] || [ "$user_input" == "y" ]; then
-echo '正在重置字体缓存中'
+echo -e "${Hong}正在重置字体缓存中${RESET_COLOR}"
 sudo fc-cache -f -v
-echo '中文字体设置完毕'
+echo -e "${Lan}中文字体设置完毕${RESET_COLOR}"
 break
 elif [ "$user_input" == "no" ] || [ "$user_input" == "n" ]; then
 break
 else
-echo "输入错误，请输入yes或no:"
+echo -e "${Hong}输入错误，请输入yes或no:${RESET_COLOR}"
 fi
 done
 
 # 安装screen
 which screen > /dev/null
 if [ $? -ne 0 ]; then
-echo "screen未安装，正在安装中..."
+echo -e "${Hong}screen未安装，正在安装中...${RESET_COLOR}"
 sudo apt install -y screen
 else
-echo "screen已安装，跳过安装步骤"
+echo -e "${Huang}screen已安装，跳过安装步骤${RESET_COLOR}"
 fi
 
 # 安装大象数据库
 if ! dpkg -l | grep -qw postgresql; then
-echo "postgresql未安装，正在安装中..."
+echo -e "${Hong}postgresql未安装，正在安装中...${RESET_COLOR}"
 sudo apt install -y postgresql postgresql-contrib
 else
-echo "postgresql已安装，跳过安装步骤"
+echo -e "${Huang}postgresql已安装，跳过安装步骤${RESET_COLOR}"
 fi
 
 # 判断redis是否启动
