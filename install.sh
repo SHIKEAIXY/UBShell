18dcd561b124314503aa7dbb5469ffabde3ee620
小雨 <zsfys225011250@qq.com>
2025-02-23 12:58:49 +0000

    细节优化
    Signed-off-by: 小雨 <zsfys225011250@qq.com>

--- install.sh
+++ install.sh
@@ -112,7 +112,7 @@ read user_input
 if [ "$user_input" == "yes" ] || [ "$user_input" == "y" ]; then
 echo -e "${Huang}正在安装中${RESET_COLOR}"
 sudo apt remove -y libnode-dev
-curl -sL https://deb.nodesource.com/setup_22.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_23.x | sudo -E bash -
 sudo apt install -y nodejs
 echo -e "${Lu}安装完毕${RESET_COLOR}"
 break
@@ -190,7 +190,7 @@ echo -e "${Lu}中文字体设置完毕${RESET_COLOR}"
 fi
 
 # 安装剩余所需包
-packages=(lsof ffmpeg screen postgresql postgresql-contrib ca-certificates fonts-liberation libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils libxkbcommon0)
+packages=(lsof ffmpeg screen postgresql postgresql-contrib ca-certificates fonts-liberation libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc-s1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils libxkbcommon0)
 for package in "${packages[@]}"; do
 if dpkg -s "$package" >/dev/null 2>&1; then
 echo -e "${Lan}${package} 已安装，跳过${RESET_COLOR}"
